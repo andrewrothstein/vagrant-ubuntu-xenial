7c9d1b5aebd76abb775cf3675746f5b3fb1a6857
Andrew Rothstein <andrew.rothstein@gmail.com>
2016-03-23 18:20:48 -0400

    generate certs!

--- initca.sh
+++ initca.sh
@@ -6,3 +6,17 @@ then
    cfssl genkey -initca ca.json | cfssljson -bare ca;
    echo done building CA;
 fi
+
+function getkey
+{
+    HOSTNAME=$1
+    if [ ! -e $HOSTNAME-key.pem ]
+    then
+	cfssl gencert -ca ca.pem -ca-key ca-key.pem -hostname=$HOSTNAME,centos-7.vagrant,'*.centos-7.vagrant' host.json | cfssljson -bare $HOSTNAME
+    fi
+}
+
+for hostid in '01' '02' '03'
+do
+    getkey centos-7-$hostid.vagrant
+done
